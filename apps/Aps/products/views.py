8c185d18f286b12bf698053b2bd0efed7c37ef69
Yessy Moreno <ynmoreno.v@gmail.com>
2016-03-10 18:13:34 -0600

    test

---
+++ apps/Aps/products/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .forms import ProductForm
+from .models import Product
+
+
+def hello_world(request):
+	product = Product.objects.order_by('id')
+	template = loader.get_template('index.html')
+	context = {
+		'product': product
+	}
+
+	return HttpResponse(template.render(context, request))
+
+def product_detail(request, pk):
+	product = get_object_or_404(Product, pk=pk)
+	template = loader.get_template('product_detail.html')
+	context = {
+		'product': product
+	}
+
+	return HttpResponse(template.render(context, request))
+
+
+def new_product(request):
+	template = loader.get_template('new_product.html')
+	context = {
+			'form': form
+	}
+
+	return HttpResponse(template.render(context, request))
